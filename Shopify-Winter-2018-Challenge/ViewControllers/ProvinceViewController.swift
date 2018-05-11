e769ff82be0080c17395293f119d1b23c061c5ab
Michael Ding <m.ding5692@gmail.com>
2018-05-11 03:53:50 -0400

    Created another VC for specifc provincial orders

---
+++ Shopify-Winter-2018-Challenge/ViewControllers/ProvinceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProvinceViewController.swift
+//  Shopify-Winter-2018-Challenge
+//
+//  Created by Michael Ding on 2018-05-11.
+//  Copyright © 2018 MDing. All rights reserved.
+//
+
+import UIKit
+
+// For showing orders to each province
+
+class ProvinceViewController: UIViewController {
+    
+}
